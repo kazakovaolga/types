6b19beaf24684e17e28e8e8c2e16d8bbb0d62037
Olga Kazakova <olegenok@bk.ru>
2022-09-05 09:11:53 +0300

    change API calendar

--- src/calendar.test.ts
+++ src/calendar.test.ts
@@ -1,4 +1,4 @@
-import { Calendar, Task } from "./calendar";
+import {  remoteStorageCalendar, Task } from "./calendar";
 
 describe("Inspect Calendar methods", () => {
     beforeEach(() => {
@@ -11,236 +11,237 @@ describe("Inspect Calendar methods", () => {
         text: 'test',
         date: new Date('02/28/2019')
     };
-    const calendar: Calendar = new Calendar(task);
+    const namespace='calendar1';
+    const calendar: remoteStorageCalendar = new remoteStorageCalendar(task,namespace);
 
     it("Calendar is a class", () => {
-        expect(new Calendar(task)).toBeInstanceOf(Calendar);
+        expect(new remoteStorageCalendar(task,namespace)).toBeInstanceOf(remoteStorageCalendar);
     });
 
-    it("empty task list", () => {
-        calendar.readAllTasks();
-        const items = calendar.readAllTasks();
+    it("empty task list", async() => {
+        // calendar.readAllTasks(namespace);
+        const items = calendar.readAllTasks(namespace);
         expect(items).toEqual([]);
     });
 
-    it("create new task and same task", () => {
-        let items = calendar.createTask(task);           
-        items[0].date = new Date(items[0].date);
-        expect(items[0]).toEqual(task);
-        //same task
-        items = calendar.createTask(task);         
-        items[0].date = new Date(items[0].date);
-        expect(items[0]).toEqual(task);
-    });
+    // it("create new task and same task", () => {
+    //     let items = calendar.createTask(task,namespace);           
+    //     items[0].date = new Date(items[0].date);
+    //     expect(items[0]).toEqual(task);
+    //     //same task
+    //     items = calendar.createTask(task,namespace);         
+    //     items[0].date = new Date(items[0].date);
+    //     expect(items[0]).toEqual(task);
+    // });
 
 
-    it("there is one task in the list", () => {
-        calendar.createTask(task);
+    // it("there is one task in the list", () => {
+    //     calendar.createTask(task,namespace);
 
-        const items = calendar.readAllTasks();
-        const size = items.length;
-        expect(size).toEqual(1);
+    //     const items = calendar.readAllTasks(namespace);
+    //     const size = items.length;
+    //     expect(size).toEqual(1);
 
-        items[size - 1].date = new Date(items[size - 1].date);
-        expect(items[size - 1]).toEqual(task);
-    });
+    //     items[size - 1].date = new Date(items[size - 1].date);
+    //     expect(items[size - 1]).toEqual(task);
+    // });
 
-    it("request a non-existent task id=2", () => {
-        calendar.createTask(task);
+    // it("request a non-existent task id=2", () => {
+    //     calendar.createTask(task,namespace);
 
-        const item = calendar.readTask(2);
-        const size = item.length;
-        expect(size).toEqual(0);
-        expect(item).toEqual([]);
-    });
+    //     const item = calendar.readTask(2,namespace);
+    //     const size = item.length;
+    //     expect(size).toEqual(0);
+    //     expect(item).toEqual([]);
+    // });
 
-    it("update task", () => {
-        calendar.createTask(task);
+    // it("update task", () => {
+    //     calendar.createTask(task,namespace);
 
-        const taskNew = {
-            id: 1,
-            category: 'game',
-            text: 'test',
-            date: new Date('02/28/2019')
-        };
+    //     const taskNew = {
+    //         id: 1,
+    //         category: 'game',
+    //         text: 'test',
+    //         date: new Date('02/28/2019')
+    //     };
 
-        const items = calendar.updateTask(taskNew);        
-        items[0].date = new Date(items[0].date);
-        expect(items[0]).toEqual(taskNew);
-    });
+    //     const items = calendar.updateTask(taskNew,namespace);        
+    //     items[0].date = new Date(items[0].date);
+    //     expect(items[0]).toEqual(taskNew);
+    // });
 
-    it("update a non-existent task", () => {
-        calendar.createTask(task);
+    // it("update a non-existent task", () => {
+    //     calendar.createTask(task,namespace);
 
-        const taskNew = {
-            id: 5,
-            category: 'work',
-            text: 'test',
-            date: new Date('02/28/2019')
-        };
+    //     const taskNew = {
+    //         id: 5,
+    //         category: 'work',
+    //         text: 'test',
+    //         date: new Date('02/28/2019')
+    //     };
 
-        const items = calendar.updateTask(taskNew);
-        let itemUpdate: Task | [] = [];
+    //     const items = calendar.updateTask(taskNew,namespace);
+    //     let itemUpdate: Task | [] = [];
 
-        items.forEach(function (item) {
-            if (item === taskNew) { itemUpdate = item };
-        }
-        );
+    //     items.forEach(function (item) {
+    //         if (item === taskNew) { itemUpdate = item };
+    //     }
+    //     );
 
-        expect(itemUpdate).toEqual([]);
-    });
+    //     expect(itemUpdate).toEqual([]);
+    // });
 
-    it("delete task id=2", () => {
-        calendar.createTask(task);
+    // it("delete task id=2", () => {
+    //     calendar.createTask(task,namespace);
 
-        const taskNew = {
-            id: 2,
-            category: 'work',
-            text: 'test',
-            date: new Date('02/28/2019')
-        };
-        calendar.createTask(taskNew);
+    //     const taskNew = {
+    //         id: 2,
+    //         category: 'work',
+    //         text: 'test',
+    //         date: new Date('02/28/2019')
+    //     };
+    //     calendar.createTask(taskNew,namespace);
 
-        const items = calendar.deleteTask(taskNew.id);
-        let itemDelete: Task | [] = [];
+    //     const items = calendar.deleteTask(taskNew.id,namespace);
+    //     let itemDelete: Task | [] = [];
 
-        items.forEach(function (item) {
-            if (item === taskNew) { itemDelete = item };
-        }
-        );
+    //     items.forEach(function (item) {
+    //         if (item === taskNew) { itemDelete = item };
+    //     }
+    //     );
 
-        expect(itemDelete).toEqual([]);
-    });
+    //     expect(itemDelete).toEqual([]);
+    // });
 
-    it("sort tasks for id", () => {
+    // it("sort tasks for id", () => {
 
-        const tasks = [{
-            id: 4,
-            category: 'work',
-            text: 'type script coding',
-            date: new Date('07/31/2022')
-        },
-        {
-            id: 2,
-            category: 'game',
-            text: 'monopoly',
-            date: new Date('07/28/2022')
-        },
-        {
-            id: 3,
-            category: 'rest',
-            text: 'holidays',
-            date: new Date('08/07/2022')
-        },
-        {
-            id: 1,
-            category: 'sport',
-            text: 'yoga',
-            date: new Date('08/02/2022')
-        }
-        ];
+    //     const tasks = [{
+    //         id: 4,
+    //         category: 'work',
+    //         text: 'type script coding',
+    //         date: new Date('07/31/2022')
+    //     },
+    //     {
+    //         id: 2,
+    //         category: 'game',
+    //         text: 'monopoly',
+    //         date: new Date('07/28/2022')
+    //     },
+    //     {
+    //         id: 3,
+    //         category: 'rest',
+    //         text: 'holidays',
+    //         date: new Date('08/07/2022')
+    //     },
+    //     {
+    //         id: 1,
+    //         category: 'sport',
+    //         text: 'yoga',
+    //         date: new Date('08/02/2022')
+    //     }
+    //     ];
 
-        tasks.forEach(function (task) {
-            calendar.createTask(task);
-        });
+    //     tasks.forEach(function (task) {
+    //         calendar.createTask(task,namespace);
+    //     });
 
-        const items = calendar.sortTask("id");
+    //     const items = calendar.sortTask("id",namespace);
 
-        items.forEach(function (item) {
-            item.date = new Date(item.date);
-        });
+    //     items.forEach(function (item) {
+    //         item.date = new Date(item.date);
+    //     });
 
-        expect(items).toEqual([{
-            id: 1,
-            category: 'sport',
-            text: 'yoga',
-            date: new Date('08/02/2022')
-        },
-        {
-            id: 2,
-            category: 'game',
-            text: 'monopoly',
-            date: new Date('07/28/2022')
-        },
-        {
-            id: 3,
-            category: 'rest',
-            text: 'holidays',
-            date: new Date('08/07/2022')
-        },
-        {
-            id: 4,
-            category: 'work',
-            text: 'type script coding',
-            date: new Date('07/31/2022')
-        }
-        ]);
-    });
+    //     expect(items).toEqual([{
+    //         id: 1,
+    //         category: 'sport',
+    //         text: 'yoga',
+    //         date: new Date('08/02/2022')
+    //     },
+    //     {
+    //         id: 2,
+    //         category: 'game',
+    //         text: 'monopoly',
+    //         date: new Date('07/28/2022')
+    //     },
+    //     {
+    //         id: 3,
+    //         category: 'rest',
+    //         text: 'holidays',
+    //         date: new Date('08/07/2022')
+    //     },
+    //     {
+    //         id: 4,
+    //         category: 'work',
+    //         text: 'type script coding',
+    //         date: new Date('07/31/2022')
+    //     }
+    //     ]);
+    // });
 
-    it("sort tasks for date", () => {
+    // it("sort tasks for date", () => {
 
-        const tasks = [{
-            id: 4,
-            category: 'work',
-            text: 'type script coding',
-            date: new Date('07/31/2022')
-        },
-        {
-            id: 2,
-            category: 'game',
-            text: 'monopoly',
-            date: new Date('07/28/2022')
-        },
-        {
-            id: 3,
-            category: 'rest',
-            text: 'holidays',
-            date: new Date('08/07/2022')
-        },
-        {
-            id: 1,
-            category: 'sport',
-            text: 'yoga',
-            date: new Date('08/02/2022')
-        }
-        ];
+    //     const tasks = [{
+    //         id: 4,
+    //         category: 'work',
+    //         text: 'type script coding',
+    //         date: new Date('07/31/2022')
+    //     },
+    //     {
+    //         id: 2,
+    //         category: 'game',
+    //         text: 'monopoly',
+    //         date: new Date('07/28/2022')
+    //     },
+    //     {
+    //         id: 3,
+    //         category: 'rest',
+    //         text: 'holidays',
+    //         date: new Date('08/07/2022')
+    //     },
+    //     {
+    //         id: 1,
+    //         category: 'sport',
+    //         text: 'yoga',
+    //         date: new Date('08/02/2022')
+    //     }
+    //     ];
 
-        tasks.forEach(function (task) {
-            calendar.createTask(task);
-        });
+    //     tasks.forEach(function (task) {
+    //         calendar.createTask(task,namespace);
+    //     });
 
-        const items = calendar.sortTask("date");
+    //     const items = calendar.sortTask("date",namespace);
 
-        items.forEach(function (item) {
-            item.date = new Date(item.date);
-        });
+    //     items.forEach(function (item) {
+    //         item.date = new Date(item.date);
+    //     });
 
-        expect(items).toEqual([
-            {
-                id: 2,
-                category: 'game',
-                text: 'monopoly',
-                date: new Date('07/28/2022')
-            },
-            {
-                id: 4,
-                category: 'work',
-                text: 'type script coding',
-                date: new Date('07/31/2022')
-            },
-            {
-                id: 1,
-                category: 'sport',
-                text: 'yoga',
-                date: new Date('08/02/2022')
-            },
+    //     expect(items).toEqual([
+    //         {
+    //             id: 2,
+    //             category: 'game',
+    //             text: 'monopoly',
+    //             date: new Date('07/28/2022')
+    //         },
+    //         {
+    //             id: 4,
+    //             category: 'work',
+    //             text: 'type script coding',
+    //             date: new Date('07/31/2022')
+    //         },
+    //         {
+    //             id: 1,
+    //             category: 'sport',
+    //             text: 'yoga',
+    //             date: new Date('08/02/2022')
+    //         },
 
-            {
-                id: 3,
-                category: 'rest',
-                text: 'holidays',
-                date: new Date('08/07/2022')
-            }
-        ]);
-    });
+    //         {
+    //             id: 3,
+    //             category: 'rest',
+    //             text: 'holidays',
+    //             date: new Date('08/07/2022')
+    //         }
+    //     ]);
+    // });
 });
--- src/calendar.ts
+++ src/calendar.ts
@@ -1,4 +1,4 @@
-const key = "tasks";
+//const key = "tasks";
 
 export interface Task {
     id: number,
@@ -7,83 +7,208 @@ export interface Task {
     date: Date
 };
 
-export class Calendar {
+export interface Calendar {
+    createTask(task: Task, namespace: string): Promise<Task[]>,
+    readAllTasks(namespace: string): Promise<Task[]>,
+    readTask(id: number, namespace: string): Promise<Task[]>,
+    updateTask(task: Task, namespace: string): Promise<Task[]>,
+    deleteTask(id: number, namespace: string): Promise<Task[]>,
+    sortTask(field: string | number, namespace: string): Promise<Task[]>
+}
+
+// export class localStorageCalendar implements Calendar {
+//     namespace: string;
+//     task: Task;
+
+//     constructor(task: Task,namespace:string) {
+//         this.task = task;
+//         this.namespace=namespace;
+//     }
+
+//     async createTask(task: Task,namespace:string) {
+//         const items=this.readAllTasks(namespace);
+//         const result = items.find(function (item) {
+//             return item.id === task.id;
+//         })        
+
+//         if (result === undefined) {            
+//             items.push(task);            
+//             localStorage.setItem(namespace, JSON.stringify(items));
+//         }
+//         return items;
+//     }
+
+//     async readAllTasks(namespace:string) {        
+//         const items = localStorage.getItem(namespace) as string;       
+//         console.log('readAllTasks items=',items) 
+//         return items === null ? [] : JSON.parse(items);
+//     }
+
+//     async readTask(id: number,namespace:string) {        
+//         const values = localStorage.getItem(namespace);
+//         if (values === null) return [];
+
+//         const items = JSON.parse(values);        
+
+//         for (let i = 0; i < items.length; i++) {
+//             if (items[i].id === id) return items[i];
+//         }        
+//         return [];
+//     }
+
+//     async updateTask(task: Task,namespace:string) {
+//         const items= this.readAllTasks(namespace);    
+//         items.find(function (item: Task) {
+//             if (item.id === task.id) {                                
+
+//                 for (const k in task) {                    
+//                     if (k in item) {
+//                         item[k] = task[k]
+//                     }
+//                 }
+//                 return true;
+//             }
+//             return false;
+//         })
+
+//         localStorage.setItem(namespace, JSON.stringify(items));
+//         return items;
+//     }
+
+//     async deleteTask(id: number,namespace:string) {
+//         const values = localStorage.getItem(namespace);        
+//         if (values === null) return [];
+//         const items: Task[] = JSON.parse(values);
+
+//         for (let i = 0; i < items.length; i++) {
+//             if (items[i].id === id) {                
+//                 items.splice(i, 1);                          
+//             }
+//         }
+//         localStorage.setItem(namespace, JSON.stringify(items));
+//         return items        
+//     }
+
+//     async sortTask(field: string | number,namespace:string) {
+//         const values = localStorage.getItem(namespace);        
+//         if (values === null) return [];
+
+//         const items: Task[] = JSON.parse(values);        
+
+//         if (field === 'date') {
+//             items.sort(function (a: Task, b: Task) {
+//                 return (new Date(a.date).getTime() - new Date(b.date).getTime());
+//             });
+
+//         }
+
+//         else {
+//             items.sort((prev, next) => {
+//                 if ((prev && next) !== null) {                    
+//                     if (prev[field] < next[field]) return -1;
+//                     if (prev[field] < next[field]) return 1;
+//                 }
+//                 return 1;
+//             });
+//         }        
+
+//         localStorage.setItem(namespace, JSON.stringify(items));
+//         return items;
+//     }
+// }
+
+export class remoteStorageCalendar implements Calendar {
+    namespace: string;
     task: Task;
 
-    constructor(task: Task) {
+    constructor(task: Task, namespace: string) {
         this.task = task;
+        this.namespace = namespace;
     }
 
-    createTask(task: Task) {
-        const items: Task[] = this.readAllTasks();
+    async createTask(task: Task, namespace: string) {
+        const items = await this.readAllTasks(namespace);
+        console.log("createTask items", items);     
         const result = items.find(function (item) {
             return item.id === task.id;
-        })        
+        })
 
-        if (result === undefined) {            
-            items.push(task);            
-            localStorage.setItem(key, JSON.stringify(items));
+        if (result === undefined) {
+            items.push(task);
+            //localStorage.setItem(namespace, JSON.stringify(items));
         }
+        let response = await fetch('/article/fetch/post/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(task)
+        });
+        let result = await response.json();
         return items;
     }
 
-    readAllTasks() {        
-        const items = localStorage.getItem(key) as string;        
-        return items === null ? [] : JSON.parse(items);
+    async readAllTasks(namespace: string) {
+        const url = `https://crudcrud.com/api/b6d6d59df9494ea0936b15dea5371496`;
+        let response = await fetch(url);
+        let items = await response.json();
+        console.log('type of items=', typeof items);
+        console.log('items=', items);
+        return items.length === 0 ? [] : JSON.parse(items);
     }
 
-    readTask(id: number) {        
-        const values = localStorage.getItem(key);
+    async readTask(id: number, namespace: string) {
+        const values = localStorage.getItem(namespace);
         if (values === null) return [];
 
-        const items = JSON.parse(values);        
+        const items = JSON.parse(values);
 
         for (let i = 0; i < items.length; i++) {
             if (items[i].id === id) return items[i];
-        }        
+        }
         return [];
     }
 
-    updateTask(task: Task) {
-        const items: Task[] = this.readAllTasks();
-        
+    async updateTask(task: Task, namespace: string) {
+        const items = this.readAllTasks(namespace);
 
-        items.find(function (item: Task) {
-            if (item.id === task.id) {                                
 
-                for (const k in task) {                    
-                    if (k in item) {
-                        item[k] = task[k]
-                    }
-                }
-                return true;
-            }
-            return false;
-        })
+        // items.find(function (item: Task) {
+        //     if (item.id === task.id) {                                
 
-        localStorage.setItem(key, JSON.stringify(items));
+        //         for (const k in task) {                    
+        //             if (k in item) {
+        //                 item[k] = task[k]
+        //             }
+        //         }
+        //         return true;
+        //     }
+        //     return false;
+        // })
+
+        localStorage.setItem(namespace, JSON.stringify(items));
         return items;
     }
 
-    deleteTask(id: number) {
-        const values = localStorage.getItem(key);        
+    async deleteTask(id: number, namespace: string) {
+        const values = localStorage.getItem(namespace);
         if (values === null) return [];
         const items: Task[] = JSON.parse(values);
 
         for (let i = 0; i < items.length; i++) {
-            if (items[i].id === id) {                
-                items.splice(i, 1);                          
+            if (items[i].id === id) {
+                items.splice(i, 1);
             }
         }
-        localStorage.setItem(key, JSON.stringify(items));
-        return items        
+        localStorage.setItem(namespace, JSON.stringify(items));
+        return items
     }
 
-    sortTask(field: string | number) {
-        const values = localStorage.getItem(key);        
+    async sortTask(field: string | number, namespace: string) {
+        const values = localStorage.getItem(namespace);
         if (values === null) return [];
 
-        const items: Task[] = JSON.parse(values);        
+        const items: Task[] = JSON.parse(values);
 
         if (field === 'date') {
             items.sort(function (a: Task, b: Task) {
@@ -94,15 +219,15 @@ export class Calendar {
 
         else {
             items.sort((prev, next) => {
-                if ((prev && next) !== null) {                    
+                if ((prev && next) !== null) {
                     if (prev[field] < next[field]) return -1;
                     if (prev[field] < next[field]) return 1;
                 }
                 return 1;
             });
-        }        
+        }
 
-        localStorage.setItem(key, JSON.stringify(items));
+        localStorage.setItem(namespace, JSON.stringify(items));
         return items;
     }
 }
